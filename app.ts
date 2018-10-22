f0b9e1b83461a90c44fd772b948ecf579f164791
Berkay GURSOY <berkaygursoy@gmail.com>
2018-10-22 21:26:21 +0300

    added onDisconnect

--- app.ts
+++ app.ts
@@ -4,28 +4,28 @@ import { Client } from './Client';
 console.log("Connecting to server...");
 
 let socket = SocketIO('http://localhost:5000');
-
 let client: Client;
 
-socket.on('connect', () => {
+socket.on('connect', onConnectServer);
+socket.on('disconnect', onDisconnectServer);
 
+function onConnectServer(): void {
     client = new Client(socket.id);
 
     console.log("Connected to server:" + client.clientId);
-    socket.emit('matchmake', client.clientId);
-    socket.on('roomJoin', joinRoom);
-    socket.on('broadcast', roomBroadcast);
-    // setTimeout(() => {
-    //     socket.emit('roomLeave',player);
-    // }, 5000);
-});
+    socket.emit('matchmake', client);
+    socket.on('roomJoin', onJoinRoom);
 
-function joinRoom(roomId: string): void {
+    setTimeout(() => {
+        socket.emit('roomLeave');
+        socket.removeListener('roomJoin', onJoinRoom);
+    }, 2000);
+}
+
+function onDisconnectServer(): void {
+    console.log("Disconncted from server");
+}
+
+function onJoinRoom(roomId: string): void {
     console.log(`Connected to room ${roomId}`);
 }
-
-function roomBroadcast(msg: string): void {
-    console.log(`Broadcast ${msg}`);
-}
-
-
