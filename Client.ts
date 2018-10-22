9e9720a0929c3d60032d1574563591fab47bb2d6
Berkay GURSOY <berkaygursoy@gmail.com>
2018-10-22 21:26:07 +0300

    readonly public Id

--- Client.ts
+++ Client.ts
@@ -1,15 +1,7 @@
-import SocketIO = require('socket.io');
-
 export class Client {
-    // private socket: SocketIO.Socket;
-    private _clientId:string;
-    // constructor(socket: SocketIO.Socket) {
-    //     this.socket = socket;
-    // }
+    public readonly clientId: string;
+
     constructor(socketId: string) {
-        this._clientId = socketId;
-    }
-    get clientId(): string {
-        return this._clientId;
+        this.clientId = socketId;
     }
 }
